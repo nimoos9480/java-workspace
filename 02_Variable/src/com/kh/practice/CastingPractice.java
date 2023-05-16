0690c74a2da30bcebbdc91470ffd120c27cd9215
nimoos9480 <133620313+nimoos9480@users.noreply.github.com>
2023-05-16 21:59:24 +0900

    CastingPractice

--- 02_Variable/src/com/kh/practice/CastingPractice.java
+++ 02_Variable/src/com/kh/practice/CastingPractice.java
@@ -5,7 +5,8 @@ public class CastingPractice {
 	public static void main(String[] args) {
 		
 		CastingPractice cp = new CastingPractice();
-		cp.method1();
+	//	cp.method1();
+		cp.method2();
 		
 	}
 	
@@ -15,7 +16,32 @@ public class CastingPractice {
 		System.out.print("문자 : ");
 		String str = sc.next();
 		
+		int num = 'A'; 
+		int num2 = 'B';
+		System.out.println("A unicode : " + num);
+		System.out.println("B unicode : " + num2);
+
+	}
+	
+	public void method2() {
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.print("국어 : ");
+		int x = sc.nextInt();
+		
+		System.out.print("영어 : ");
+		int y = sc.nextInt();
+		
+		System.out.print("수학 : ");
+		int z = sc.nextInt();
+		
+		int sum = (x + y + z);
+		double avg = sum/3.0;
+		
+		System.out.printf("총점 : %d\n평균 : %.2f", sum, avg); 
+
+
+		// 37 45 33
 	}
 
 
